b2ade215fb6c2adad31a5f62879b2992e2b57c34
ale-rt <alessandro.pisa@gmail.com>
2021-03-18 17:23:35 +0100

    Back to development: 1.2.2

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import find_packages
 from setuptools import setup
 
 
-version = "1.2.1"
+version = "1.2.2.dev0"
 description = "Memcached cache manager for Zope."
 long_description = open("README.rst").read() + "\n" + open("CHANGES.rst").read()
 
